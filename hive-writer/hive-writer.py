d976909382be24b3acffc19abda0e59786d8fae5
Brian of London <bol@snnsite.com>
2021-05-06 13:26:15 +0300

    Changed account_id to opperation_id

--- hive-writer/hive-writer.py
+++ hive-writer/hive-writer.py
@@ -47,7 +47,7 @@ def send_notification(custom_json, server_account='', wif=''):
         HIVE_POSTING_KEY
         """
 
-    account_id = 'hive-hydra'
+    opperation_id = 'hive-hydra'
 
     try:
         if server_account == '':
@@ -63,7 +63,7 @@ def send_notification(custom_json, server_account='', wif=''):
             h = Hive(keys=wif)
 
 
-        tx = h.custom_json(id=account_id, json_data= custom_json,
+        tx = h.custom_json(id=opperation_id, json_data= custom_json,
                             required_posting_auths=[server_account])
 
         trx_id = tx['trx_id']
@@ -78,7 +78,7 @@ def send_notification(custom_json, server_account='', wif=''):
 
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 5000
+    HOST, PORT = "localhost", 5001
 
     # Create the server, binding to localhost on port 9999
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
