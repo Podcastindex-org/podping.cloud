d2229c89200df9c75b909f36b4046aba59161471
Jeremy D. Gerdes <seakintruth@gmail.com>
2021-05-19 10:05:09 -0400

    Comment cleanup and removed write_to_file

--- hive-watcher/examples/out-as-csv/hive-watcher.py
+++ hive-watcher/examples/out-as-csv/hive-watcher.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-# out-as-csv/hive-watcher.py
+# Modeled after the simple-watcher.py V1.0
+# modified to print to STDOUT as a comma seperated value (.csv) stream
 #
 # Simple version of Hive Podping watcher - no options, just runs
-# exports all post content in csv format
 # The only external library needed is "beem" - pip install beem
 # Beem is the official Hive accessing library for Python.
 #
@@ -29,12 +29,8 @@ def get_allowed_accounts(acc_name="podping") -> Set[str]:
     # This is giving an error if I don't specify api server exactly.
     # TODO reported as Issue on Beem library https://github.com/holgern/beem/issues/301
     h = beem.Hive(node="https://api.hive.blog")
-
     master_account = Account(acc_name, blockchain_instance=h, lazy=True)
-
     return set(master_account.get_following())
-
-
 
 def allowed_op_id(operation_id) -> bool:
     """Checks if the operation_id is in the allowed list"""
@@ -51,21 +47,10 @@ def configure_logging():
             os.mkdir(log_dir)
         logging.basicConfig(filename=os.path.join(log_dir,"errors-"+log_name), encoding='utf-8', level=logging.ERROR)
     except:
-        logging.basicConfig(filename="errors-"+log_name, encoding='utf-8', level=logging.ERROR)
-
-def write_to_file(content,filepath):
-    try: # if filepath passed is just a file name we expect an error here
-        if not os.path.isdir(os.path.dirname(filepath)):
-            os.mkdirs(os.path.dirname(filepath))
-    except:
-        logging.error(sys.exc_info()[0])
-    if not os.path.isfile(filepath):
-        open(filepath, 'w').close()
-
-    # Open for appending and write to log
-    f = open(filepath, "a")
-    f.write(content)
-    f.close()
+        try:
+            logging.basicConfig(filename="errors-"+log_name, encoding='utf-8', level=logging.ERROR)
+        except:
+            logging.basicConfig(filename="errors.log", encoding='utf-8', level=logging.ERROR)
 
 def main():
     """ Outputs URLs one by one as they appear on the Hive Podping stream """
@@ -81,15 +66,12 @@ def main():
         opNames=["custom_json"], raw_ops=False, threading=False, thread_num=4
     )
     configure_logging()
-    # Calling script should create file with header of
-    # print('timestamp_seen,timestamp_post,id,trx_id,trx_num,type,block_num,required_auths,required_posting_auths,"json"')
     for post in stream:
         try:
              # Filter only on post ID from the list above.
              if allowed_op_id(post["id"]):
                  # Filter by the accounts we have authorised to podping
                  if set(post["required_posting_auths"]) & allowed_accounts:
-#                     write_to_file(json.dumps(data, indent=4),'~/watched_json_files/'+time.time()+'.json')
                      print(repr(time.time())+','+repr(post.get("timestamp").timestamp())+','+ \
                          str(post.get("id"))+','+str(post.get("trx_id"))+','+repr(post.get("trx_num"))+','+ \
                          repr(post.get("block_num"))+','+repr(post.get("required_auths"))+','+ \
@@ -101,4 +83,3 @@ def main():
 if __name__ == "__main__":
     # Runs until terminated with Ctrl-C
     main()
-
