ff1a5081b84a453293147466bf474c16e7a97200
Jeremy D. Gerdes <seakintruth@gmail.com>
2021-05-19 10:51:42 -0400

    Add files via upload

--- hive-watcher/examples/out-as-csv/visualize-data.R
+++ hive-watcher/examples/out-as-csv/visualize-data.R
@@ -8,11 +8,20 @@
 # Version 0.1
 podping_data <- read.csv("data.csv")
 # for a vector image could use: postscript(file="image-timestamp_delay.ps")
+time_stamp_delays <- podping_data$timestamp_seen-podping_data$timestamp_post
 png(file="image-timestamp_delay.png",
-    width=900, height=900)
+    width=600, height=600)
 plot(
-	x=podping_data$timestamp_post,
-	y=podping_data$timestamp_seen-podping_data$timestamp_post
+  x=podping_data$timestamp_post,
+  y=time_stamp_delays,
+  main="Scatter plot of watcher delay in seconds"
+)
+dev.off()
+png(file="image-timestamp_delay_hist.png",
+    width=600, height=600)
+hist(
+  time_stamp_delays,
+  main="Histogram of watcher delay in seconds"
 )
 dev.off()
 
