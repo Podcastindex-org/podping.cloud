c1443171c9475f8de7165498714c680baaf4584e
Jeremy D. Gerdes <seakintruth@gmail.com>
2021-05-19 23:45:51 -0400

    Added data files to .gitignore, and more stats

--- hive-watcher/examples/out-as-csv/visualize-data.R
+++ hive-watcher/examples/out-as-csv/visualize-data.R
@@ -1,12 +1,50 @@
 #!/usr/bin/env Rscript
-# A place holder script for data exploration and visualization
-# (that actually does a little visualization)
-#
 # Requires installation of R, at a minimum for apt use:
 #     $sudo apt install r-base r-base-core r-recommended
-#
 # Version 0.1
-podping_data <- read.csv("data.csv")
+# starting descriptive stats with
+# https://bookdown.org/wadetroberts/bookdown-demo/descriptive-statistics-and-data-visualization.html
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  psych, ggplot2, table1, patchwork,
+  data.table,dplyr,tidyverse, anytime
+)
+
+if (file.exists("data.csv")) {
+  podping_data <- fread(file="data.csv") 
+}
+if (file.exists("data-unauthorized.csv")) {
+  podping_unathorized_data <- fread(file="data-unauthorized.csv")
+}
+if (file.exists("data-not-podping_firehose.csv")) {
+  not_podping_data <- fread(file="data-not-podping_firehose.csv")
+}
+count_not_podping_data_unique <- data.table::uniqueN(not_podping_data)
+count_podping_data_unique <- data.table::uniqueN(podping_data)
+minutes_watching <- (
+  (max(podping_data$timestamp_seen)-min(podping_data$timestamp_seen)) / (60)
+)
+message(
+  "Hive watchers 'custom json' post (posts can contain multiple feed urls) counts: \n\t From ",
+  anytime(min(podping_data$timestamp_seen)),
+  " to ",
+  anytime(max(podping_data$timestamp_seen)),
+  "\n\t podping post count = ",
+  count_podping_data_unique, 
+  " (", round(count_podping_data_unique/minutes_watching,2),
+  " posts/min)",
+  "\n\t all other post count = ",
+  count_not_podping_data_unique,
+  " (", round(count_not_podping_data_unique/minutes_watching,2),
+  " posts/min)",
+  "\n\t podping portion of all posts on hive is %",
+  round(
+    100 * count_podping_data_unique / 
+    (count_podping_data_unique+count_not_podping_data_unique),
+    5
+  )
+)
+
 # for a vector image could use: postscript(file="image-timestamp_delay.ps")
 time_stamp_delays <- podping_data$timestamp_seen-podping_data$timestamp_post
 png(file="image-timestamp_delay.png",
@@ -25,3 +63,19 @@ hist(
 )
 dev.off()
 
+time_stamp_delays <- not_podping_data$timestamp_seen-not_podping_data$timestamp_post
+png(file="image-timestamp_delay-non-podping.png",
+    width=600, height=600)
+plot(
+  x=not_podping_data$timestamp_post,
+  y=time_stamp_delays,
+  main="Scatter plot of watcher delay in seconds - non-podping posts"
+)
+dev.off()
+png(file="image-timestamp_delay_hist-non-podping.png",
+    width=600, height=600)
+hist(
+  time_stamp_delays,
+  main="Histogram of watcher delay in seconds - non-podping posts"
+)
+dev.off()
