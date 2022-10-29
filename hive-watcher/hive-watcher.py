93edb8feb1ee19d1db795e3fcc9afb0195d0ba2a
Alecks Gates <alecks@podping.org>
2022-10-29 02:17:16 -0500

    Re-add hive-api.arcange.eu

--- hive-watcher/hive-watcher.py
+++ hive-watcher/hive-watcher.py
@@ -34,7 +34,7 @@ def get_client(
         nodes = [
             "https://api.hive.blog",
             "https://api.deathwing.me",
-            #"https://hive-api.arcange.eu",
+            "https://hive-api.arcange.eu",
             "https://api.openhive.network",
         ]
         client = Client(
