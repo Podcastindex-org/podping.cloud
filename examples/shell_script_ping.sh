78d636cf4ebcdf45fe5e24249d0f911ad40d63a0
Dave Jones <sect62@gmail.com>
2022-11-16 23:36:19 -0600

    example tweak

--- examples/shell_script_ping.sh
+++ examples/shell_script_ping.sh
@@ -4,4 +4,4 @@ export PPUSERAGENT="Awesome Hosting Company"
 export PPAUTHORIZATION="31D0T5IwXp6UNpNyK6r2i52RYucZJKYXUPpSey6YWK8t"
 
 
-wget --header="User-Agent: $PPUSERAGENT" --header="Authorization: $PPAUTHORIZATION" https://podping.cloud/?url=http://feed.nashownotes.com/rss.xml -O/dev/null
+wget --header="User-Agent: $PPUSERAGENT" --header="Authorization: $PPAUTHORIZATION" https://podping.cloud/?url=http://feeds.example.com/podcast.xml -O/dev/null
