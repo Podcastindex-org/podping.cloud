43eedfc93db98463fcb0992df84b77854881a1fc
Dave Jones <sect62@gmail.com>
2022-11-29 12:05:22 -0600

    stresser - random url generation

--- stresser/src/main.rs
+++ stresser/src/main.rs
@@ -4,15 +4,16 @@
 // the live, public system.  The urls.txt is just one url per line.
 //
 
-use std::io::prelude::*;
-use std::fs::File;
-use std::io::BufReader;
+// use std::io::prelude::*;
+// use std::fs::File;
+// use std::io::BufReader;
 use std::env;
 use std::process;
 use futures::stream::StreamExt;
 use rand::Rng;
-use std::time;
-use std::thread;
+//use std::time;
+//use std::thread;
+
 
 const AUTH_HEADER_TOKEN: &str = "Blahblah^^12345678";
 const USAGE_TEXT: &str = "stresser [hostname|ip] [url count]";
@@ -41,18 +42,9 @@ const MEDIUMS: [&'static str; 14] = [
 ];
 
 
-fn read_lines(path: &str) -> std::io::Result<Vec<String>> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    Ok(
-        reader.lines().filter_map(Result::ok).collect()
-    )
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    let paths: Vec<String> = read_lines("urls.txt")?;
 
     let mut hostname = "";
     if let Some(arg_host) = &args.get(1) {
@@ -64,18 +56,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Host: [{}].", hostname);
 
     //Urls to send
-    let mut url_count = 5;
+    let mut url_count: usize = 5;
     if let Ok(arg_count) = args[2].parse() {
         url_count = arg_count;
     }
     println!("Sending: [{}] urls.", url_count);
 
+    let paths = build_url_list(url_count);
+
     let fetches = futures::stream::iter(
         paths[..url_count].into_iter().map(|path| {
+            //thread::sleep(time::Duration::from_millis(rand::thread_rng().gen_range(50..300)));
             async move {
                 //Generate a random reason/medium
-                let reason = REASONS.get(rand::thread_rng().gen_range(0..2)).unwrap();
-                let medium = MEDIUMS.get(rand::thread_rng().gen_range(0..13)).unwrap();
+                let reason = REASONS.get(0).unwrap();
+                let medium = MEDIUMS.get(0).unwrap();
 
                 //Build the request url
                 let pp_get_url = format!("http://{}/?url={}&medium={}&reason={}",
@@ -85,8 +80,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                                          reason,
                 );
                 println!("Sending: [{}]...", pp_get_url);
-
-                thread::sleep(time::Duration::from_millis(rand::thread_rng().gen_range(50..300)));
 
                 let client = reqwest::Client::new();
                 match client.get(&pp_get_url)
@@ -110,3 +103,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     fetches.await;
     Ok(())
 }
+
+fn build_url_list(count: usize) -> Vec<String> {
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const URL_LEN: usize = 30;
+    let mut rng = rand::thread_rng();
+
+    let mut url_list: Vec<String> = vec!();
+
+    for _ in 0..count {
+        let postfix: String = (0..URL_LEN)
+            .map(|_| {
+                let idx = rng.gen_range(0..CHARSET.len());
+                CHARSET[idx] as char
+            })
+            .collect();
+
+        url_list.push(format!("https://example.com/{}/rss", postfix));
+    }
+
+    return url_list;
+}
+
+// fn read_lines(path: &str) -> std::io::Result<Vec<String>> {
+//     let file = File::open(path)?;
+//     let reader = BufReader::new(file);
+//     Ok(
+//         reader.lines().filter_map(Result::ok).collect()
+//     )
+// }
