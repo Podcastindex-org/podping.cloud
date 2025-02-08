c03a89190a61067d940022fcedc22d3852f33637
Dave Jones <sect62@gmail.com>
2025-02-08 16:57:16 -0600

    update docker compose with new image tags

---
+++ docker/multi-arch-builder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx rm builder
+docker buildx create --name builder --driver docker-container --use
+docker buildx inspect --bootstrap
+sudo docker buildx build --platform linux/amd64 --tag podcastindexorg/podcasting20-podping.cloud:v0.2.2 --tag podcastindexorg/podcasting20-podping.cloud:latest --output "type=registry" .
