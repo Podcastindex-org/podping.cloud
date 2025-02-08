da9a5b23e04737b259fdf41d3c3ed764163936b3
Dave Jones <sect62@gmail.com>
2025-02-08 17:23:47 -0600

    update dockerfile to use debian bookworm slim

--- docker/multi-arch-builder.sh
+++ docker/multi-arch-builder.sh
@@ -4,4 +4,4 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx rm builder
 docker buildx create --name builder --driver docker-container --use
 docker buildx inspect --bootstrap
-sudo docker buildx build --platform linux/amd64 --tag podcastindexorg/podcasting20-podping.cloud:v0.2.2 --tag podcastindexorg/podcasting20-podping.cloud:latest --output "type=registry" .
+sudo docker buildx build --platform linux/amd64 --tag podcastindexorg/podcasting20-podping.cloud:v0.2.2 --tag podcastindexorg/podcasting20-podping.cloud:latest --no-cache --output "type=registry" .
